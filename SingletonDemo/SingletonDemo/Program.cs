d696eece6559c251244f16ec5577d5a734a32173
Gauravtripathi12345 <tripathigaurav38@gmail.com>
2024-02-07 10:44:26 +0530

    Implementing Singleton Class

---
+++ SingletonDemo/SingletonDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // instance of the SingletonClass
+            SingletonClass singleton1 = SingletonClass.Instance;
+            SingletonClass singleton2 = SingletonClass.Instance;
+
+            Console.WriteLine(singleton1 == singleton2);
+
+            string username = Username();
+            singleton2.Greeting(username);
+
+            Console.ReadLine();
+        }
+
+        public static string Username()
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            return name;
+        }
+    }
+}
