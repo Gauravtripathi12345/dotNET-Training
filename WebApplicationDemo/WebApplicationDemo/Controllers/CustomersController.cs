c4fba50f124044701575cb6c5ed21ab32efee0e9
Gauravtripathi12345 <tripathigaurav38@gmail.com>
2024-02-08 13:52:53 +0530

    Implementing Basics of MVC (Controller and Views)

---
+++ WebApplicationDemo/WebApplicationDemo/Controllers/CustomersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationDemo.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Login()
+        {
+            return Content("This is the login page");
+            //return View();
+        }
+    }
+}
