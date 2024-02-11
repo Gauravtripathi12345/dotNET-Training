0ff2587fa6734bc4b08af58d741a1b9a5a3ff0e5
Gauravtripathi12345 <tripathigaurav38@gmail.com>
2024-02-11 09:00:39 +0530

    Implementing ActionResult and its common methods, Passing data from COntroller to View

--- WebApplicationDemo/WebApplicationDemo/Controllers/CustomersController.cs
+++ WebApplicationDemo/WebApplicationDemo/Controllers/CustomersController.cs
@@ -14,5 +14,55 @@ namespace WebApplicationDemo.Controllers
             return Content("This is the login page");
             //return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(string userid, string password)
+        {
+            if (userid.Length == 0 && password.Length == 0)
+            {
+                return Content("Error");
+            }
+            else if (userid == "Ankita" && password == "Ankita@123")
+            {
+                return Content("Userid and password is valid");
+            }
+            else
+            {
+                return Content("Userid and password is valid");
+            }
+        }
+
+        public ActionResult Register()
+        {
+            //return Content("This is the registration screen");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string name, string city, string email)
+        {
+            if (name.Length > 0 && city.Length > 0 && email.Length > 0)
+            {
+                return RedirectToAction("Cart");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Cart() 
+        { 
+            return View(); 
+        }
+
+        [HttpPost]
+        public ActionResult Cart(int id)
+        {
+            return RedirectToAction("ProductList", "Products");
+        }
+
     }
+
+    
 }
---
+++ WebApplicationDemo/WebApplicationDemo/Models/ElectronicProducts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationDemo.Models
+{
+    public class ElectronicProducts
+    {
+        public int Productid { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+    }
+}
